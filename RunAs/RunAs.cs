fe22473f6510ff9e561d5a4f251ac72860a15ee3
Ori <oribenhur@gmail.com>
2017-01-04 01:24:14 +0200

    Initial commit

---
+++ RunAs/RunAs.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security;
+
+namespace RunAs
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string username = "", password = "", domain = "", apppath = "", arguments = "";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i].ToLower())
+                {
+                    case "-u":
+                        username = args[++i];
+                        break;
+                    case "-p":
+                        password = args[++i];
+                        break;
+                    case "-d":
+                        domain = args[++i];
+                        break;
+                    default:
+                        if (apppath == "")
+                            apppath = args[i];
+                        else
+                            arguments += args[i] + (i < args.Length ? " " : "");
+                        break;
+                }
+            }
+
+            if (args.Length == 0 || apppath == "")
+            {
+                Console.WriteLine("\nCommand line syntax:\nRunAs [-u user] [-p password] [-d domain] Program.exe  [Program params (if exsit)]\n");
+                return 1;
+            }
+            Console.WriteLine(arguments);
+            return RunAs(apppath, arguments, domain, username, password);
+
+        }
+        static int RunAs(string apppath, string arguments, string domain, string username, string password)
+        {
+            try
+            {
+                var userProcess = Process.Start(apppath, arguments, username, GetSecure(password), domain);
+                while (userProcess != null && !userProcess.HasExited)
+                {
+                }
+                if (userProcess != null) return userProcess.ExitCode;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return -1;
+        }
+
+        static SecureString GetSecure(string str)
+        {
+            SecureString secureStr = new SecureString();
+            foreach (var c in str)
+            {
+                secureStr.AppendChar(c);
+            }
+
+            return secureStr;
+        }
+    }
+}
